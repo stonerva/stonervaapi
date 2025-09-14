cba724878ca11ac6e29f3caa53b41766ea9307ab
Soummya Biswas <soummyabiswas999@gmail.com>
2025-09-14 23:54:03 +0530

    starting products apis

---
+++ controllers/admin-apis/admincontroller.js
@@ -0,0 +1,27 @@
+import Products from '../../models/adminSchema/productModel.js'
+
+
+const addProducts = async(req,res)=>{
+
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const getAllProducts = async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const updateProduct = (req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
---
+++ models/adminSchema/productModel.js
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose'
+
+const productsSchema = new mongoose.Schema({
+
+    productId: {
+        type: String,
+        required: true
+    },
+    adminId:{
+        type:String,
+        required:true
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    descriptiom: {
+        type: String,
+        default: "",
+    },
+    selling_price: {
+        type: Number,
+        required:true
+    },
+    buy_price:{
+        type:Number,
+        require:true
+    },
+    profit_price:{
+        type:Number,
+        require:true
+    },
+    cat_id:{
+        type:String,
+        required:true
+    },
+    thumbnail:{
+        type:String
+    },
+    otherimages:{
+        type:Array
+    },
+
+    stock:{
+        type:Number,
+        required:true
+    },
+    active:{
+        type:Number,
+        default:1
+    },
+    created_at: {
+        type: Date,
+        default: () => {
+            return Date.now();
+        },
+        immutable: true
+    },
+    updated_at: {
+        type: Date,
+        default: () => {
+            return Date.now();
+        }
+    }
+})
+
+export default mongoose.model('products', productsSchema);
